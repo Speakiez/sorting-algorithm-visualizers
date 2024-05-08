9cc368da39c5a9568b1d485a3c00fd88c695c039
Speakiez <aerholgocalin57@gmail.com>
2024-05-08 15:55:05 +0800

    Add styling to index.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 
-function App() {
+function Options() {
   return (
     <>
-      <h1>Sorting Algorithm Visualizers</h1>
+      <div ></div>
     </>
-  )
+  );
 }
 
-export default App
+export default function App() {
+  return (
+    <div id="app-content">
+      <h1>Sorting Algorithm Visualizers</h1>
+    </div>
+  );
+}
